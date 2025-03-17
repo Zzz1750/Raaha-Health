d8ae96bec6b88f731f7e803c108f00bae1e6a4e5
Vyshnav-N-P <vyshnavnp666@gmail.com>
2025-03-17 22:46:30 +0530

    Server

--- Server/index.js
+++ Server/index.js
@@ -3,11 +3,25 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const connectDB= async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected");
+    }catch(err){
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+connectDB();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/User/Sigup" , require("./Routes/user_routes/signup"));
+app.use("/User/Signup" , require("./Routes/user_routes/signup"));
 app.get("/", async (req, res) => {
     res.send("Hello, World!");
 })
