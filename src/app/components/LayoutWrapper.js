1bcef3e6014833e1190d9f0273da07c40f22ba73
Vyshnav-N-P <vyshnavnp666@gmail.com>
2025-04-11 14:52:14 +0530

    Frontend check

--- src/app/components/LayoutWrapper.js
+++ src/app/components/LayoutWrapper.js
@@ -6,9 +6,6 @@ import Footer from "./Footer";
 export default function LayoutWrapper({ children }) {
   const pathname = usePathname(); 
 
-<<<<<<< HEAD
-  const excludedRoutes = ["/privacy_policy","/cancellation_policy","/tc","/login","/signup","/specialists"];
-=======
   const staticExcludedRoutes = ["/privacy_policy","/cancellation_policy","/tc","/login","/signup" ,"/session-summary","specialist","/specialist/[id]","/specialist/[id]/[name]"];
 
    // Define regex patterns for dynamic routes
@@ -19,7 +16,6 @@ export default function LayoutWrapper({ children }) {
   staticExcludedRoutes.includes(pathname) ||
   dynamicExcludedRoutes.some((regex) => regex.test(pathname));
 
->>>>>>> 24c0496a0bb13b0e755ea8886fa9c5da4d6008fa
   return (
     <>
       {!isExcluded && <Header />}
