f5a482d3ad26802ceb45e54e0b1b8f6dbcbe50f3
Vyshnav-N-P <vyshnavnp666@gmail.com>
2025-04-11 15:05:38 +0530

    check

--- src/app/specialists/page.js
+++ src/app/specialists/page.js
@@ -1,29 +1,4 @@
 "use client"
-<<<<<<< HEAD
-import { useState } from "react";
-import ProfileCard from './components/header';
-import ProfileHeaderTabs from './components/hero';
-import QualificationsComponent from './components/qualification';
-import AboutComponent from "./components/about";
-import SessionsComponent from "./components/session";
-
-
-export default function Home() {
-  const [activeTab, setActiveTab] = useState("about");
-
-  return (
-    <main className="min-h-screen bg-white">
-      <ProfileCard />
-      <div className="mt-[300px]">
-        <ProfileHeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
-        <div className="max-w-4xl  px-4 py-8 mt-16">
-          {activeTab === "qualifications" && <QualificationsComponent />}
-          {activeTab === "about" && < AboutComponent />}
-          {activeTab === "sessions" && <SessionsComponent />}
-        </div>
-      </div>
-    </main>
-=======
 import React , {useState , useEffect} from 'react';
 import {TherapistCard} from './components/therapistCard';
 import {HeroSection} from './components/heroSection';
@@ -31,18 +6,35 @@ import {Filter} from './components/filter';
 import { useSelector } from 'react-redux';
 import {getAllDoctors} from '../../SERVICE/doctorService'
 export default function Home() {
-  const [activeTab, setActiveTab] = useState("about");
 
+  const [doctors, setDoctors] = useState();
+  const token = useSelector((state) => state.auth.accessToken);
+  const fetchDoctors = async () => {
+    try {
+      const response = await getAllDoctors(token);
+      setDoctors(response);
+      console.log("Doctors fetched successfully:", response);
+    } catch (error) {
+      console.error("Error fetching doctors:", error);
+      
+    }
+  }
+  useEffect(() => {
+    if (!doctors) {
+      fetchDoctors();
+    }
+  });
+  
   return (
-    <main className="min-h-screen bg-white">
-      <ProfileCard />
-      <div className="mt-[300px]">
-        <ProfileHeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
-        <div className="max-w-4xl  px-4 py-8 mt-16">
-          {activeTab === "qualifications" && <QualificationsComponent />}
-          {activeTab === "about" && < AboutComponent />}
-          {activeTab === "sessions" && <SessionsComponent />}
-        </div>
+    <div>
+      <HeroSection />
+      <div className="w-full px-4 sm:px-8 pt-8">
+        <Filter />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 sm:p-10 md:pl-40">
+        {doctors?.map((doctor) => (
+          <TherapistCard key={doctor._id} doctor={doctor} />
+        ))}
       </div>
     </div>
   );
